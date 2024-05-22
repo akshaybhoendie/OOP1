5017ce19b50ef0f9a8e89eb2581ca6f09c42f0f0
Akhsaykumar Bhoendie <abhoendie@qualogy.com>
2024-05-22 19:15:08 -0300

    OOP1 lessen: fizzbuzz met for loop

--- src/com/unasat/fizzbuzz/FizzBuzz.java
+++ src/com/unasat/fizzbuzz/FizzBuzz.java
@@ -3,12 +3,17 @@ package com.unasat.fizzbuzz;
 public class FizzBuzz {
 
     public static void main(String[] args) {
+
         //Manier 1
         System.out.println("Manier 1 : " + fizzBuzz(15));
 
         //Manier 2
         String fizzbuzz = fizzBuzz(21);
         System.out.println("Manier 2 : " +fizzbuzz);
+
+        //Method overload, validate FizzBuzz from 0 to 100
+
+        System.out.println(fizzBuzz());
     }
 
     static String fizzBuzz(Integer i) {
@@ -24,4 +29,23 @@ public class FizzBuzz {
         }
         return result;
     }
+    static String fizzBuzz() {
+        for (int i=0; i<100; i++) {
+            String result = "";
+            if (i % 3 == 0) {
+                result += "Fizz";
+            }
+            if (i % 5 == 0) {
+                result += "Buzz";
+            }
+            if (result.equals("")) {
+                result = String.valueOf(i);
+            }
+
+            return result;
+        }
+
+        return null;
+    }
+
 }
