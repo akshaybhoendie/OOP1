741eb80c21220123b0728155e8b870a321432ab1
Akhsaykumar Bhoendie <abhoendie@qualogy.com>
2024-05-24 19:44:39 -0300

    OOP1 lessen: fizzbuzz met for loop manier 2

--- src/com/unasat/fizzbuzz/FizzBuzz.java
+++ src/com/unasat/fizzbuzz/FizzBuzz.java
@@ -5,15 +5,17 @@ public class FizzBuzz {
     public static void main(String[] args) {
 
         //Manier 1
-        System.out.println("Manier 1 : " + fizzBuzz(15));
+        //System.out.println("Manier 1 : " + fizzBuzz(15));
 
         //Manier 2
-        String fizzbuzz = fizzBuzz(21);
-        System.out.println("Manier 2 : " +fizzbuzz);
+        //String fizzbuzz = fizzBuzz(21);
+        //System.out.println("Manier 2 : " +fizzbuzz);
 
-        //Method overload, validate FizzBuzz from 0 to 100
+        //Manier 1: validate FizzBuzz from 0 to 100
+        //fizzBuzz();
 
-        fizzBuzz();
+        //Manier 2: validate FizzBuzz from 0 to 100
+        System.out.println(fizzBuzz_2());
     }
 
     static String fizzBuzz(Integer i) {
@@ -29,8 +31,7 @@ public class FizzBuzz {
         }
         return result;
     }
-    static String fizzBuzz() {
-
+    static void fizzBuzz() {
         for (int i=0; i<100; i++) {
             String result = "";
             if (i % 3 == 0) {
@@ -45,8 +46,27 @@ public class FizzBuzz {
 
             System.out.println(result + "\n");
         }
+    }
 
-        return "Done";
+    static String fizzBuzz_2() {
+        StringBuilder endResult = new StringBuilder("");
+        for (int i=0; i<100; i++) {
+            String result = "";
+            if (i % 3 == 0) {
+                result += "Fizz";
+            }
+            if (i % 5 == 0) {
+                result += "Buzz";
+            }
+            if (result.equals("")) {
+                result = String.valueOf(i);
+            }
+
+//            System.out.println(result + "\n");
+            endResult.append(result).append("\n");
+        }
+
+        return endResult.toString();
     }
 
 }
